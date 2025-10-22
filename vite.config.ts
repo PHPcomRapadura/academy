7ae173c0bf709b607a034f7e319644c83ec156e8
William Correa <root@wilcorrea.dev>
2025-10-21 21:48:35 -0300

    feat: configure deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/calendary/" : "/",
+  base: mode === "production" ? "/academy/" : "/",
   server: {
     host: "::",
     port: 8080,
