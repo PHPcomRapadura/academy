dd610b4d2dda314d3334feab4d17cbc8736bd3ba
William Correa <root@wilcorrea.dev>
2025-10-25 11:08:00 -0300

    Change base path for gh-pages mode

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/academy/" : "/",
+  base: mode === "gh-pages" ? "/academy/" : "/",
   server: {
     host: "::",
     port: 8080,
